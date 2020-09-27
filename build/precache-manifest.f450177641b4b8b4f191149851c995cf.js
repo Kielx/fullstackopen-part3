d4990b4929199bbe1ca4c7de5180b9a74d2841c9
Kielx <kiel66@gmail.com>
2020-09-27 14:06:33 +0200

    add build

---
+++ build/precache-manifest.f450177641b4b8b4f191149851c995cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23d9a3593bd6a930ec0da3f6a73fbb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b121c853dc537c42190",
+    "url": "/static/css/main.eaf96743.chunk.css"
+  },
+  {
+    "revision": "e86fb7b6ac9dcb64d511",
+    "url": "/static/js/2.66757c66.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.66757c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b121c853dc537c42190",
+    "url": "/static/js/main.9a8cb5a6.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
