7997dc8e251edb37b7c8070a7f0a048955ab71b0
Kielx <kiel66@gmail.com>
2020-09-30 20:29:48 +0200

    use production db for deployment

--- mongo.js
+++ mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI_DEV, {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
