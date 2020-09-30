a40ca14fc67fb02acef26140ebf620b9aa39d0e1
Kielx <kiel66@gmail.com>
2020-09-30 20:42:24 +0200

    properly adress mongoDB connection by ensuring that database connection refers to dev when node_ENV is set to development, and to production DB if it is in production mode

--- mongo.js
+++ mongo.js
@@ -1,11 +1,16 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-  })
+  .connect(
+    process.env.NODE_ENV === "production"
+      ? process.env.MONGODB_URI
+      : process.env.MONGODB_URI_DEV,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  )
   .catch((err) => {
     console.log("Error while connecting", err);
   });
